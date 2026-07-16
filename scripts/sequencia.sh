6851bec8740a83de9505d22980d993f980908036
André Souza <contatosouzaandre@gmail.com>
2026-07-16 17:22:38 -0300

    Update script paths to use HOME variable

--- scripts/sequencia.sh
+++ scripts/sequencia.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # sequencia.sh -- executa scripts
 
-#/home/andre/.config/scripts/random-wallpaper.sh
-/home/andre/.config/scripts/colors/walstart.sh
+$HOME/.config/scripts/colors/walstart.sh
 sleep 4
-/home/andre/.config/scripts/colors/cava-colors.sh
-/home/andre/.config/scripts/colors/fuzzel-colors.sh
-/home/andre/.config/scripts/colors/hypr-colors.sh
-/home/andre/.config/scripts/colors/dunst-colors.sh
-#/home/andre/.config/scripts/colors/update-hyprlock.sh
-/home/andre/.config/scripts/colors/vesktop-colors.sh
-/home/andre/.config/scripts/colors/telegram-colors.sh
+$HOME/.config/scripts/colors/cava-colors.sh
+$HOME/.config/scripts/colors/fuzzel-colors.sh
+$HOME/.config/scripts/colors/hypr-colors.sh
+$HOME/.config/scripts/colors/dunst-colors.sh
+$HOME/.config/scripts/colors/update-hyprlock.sh
+$HOME/.config/scripts/colors/vesktop-colors.sh
+$HOME/.config/scripts/colors/telegram-colors.sh
