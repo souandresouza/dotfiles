b375bc1ec971120c568943de3cd3a381af370d8a
souandresouza <contatosouzaandre@gmail.com>
2026-08-30 22:32:12 -0300

    300826223208

--- scripts/sequencia.sh
+++ scripts/sequencia.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # sequencia.sh -- executa scripts
 
-$HOME/.config/scripts/walgen.sh
+#$HOME/.config/scripts/walgen.sh
 sleep 5
 $HOME/.config/scripts/colors/cava-colors.sh
 $HOME/.config/scripts/colors/fuzzel-colors.sh
 $HOME/.config/scripts/colors/kitty-colors.sh
 $HOME/.config/scripts/colors/mango-colors.sh
 $HOME/.config/scripts/colors/mako-colors.sh
-#$HOME/.config/scripts/colors/update-hyprlock.sh
+$HOME/.config/scripts/colors/update-hyprlock.sh
 #$HOME/.config/scripts/colors/vesktop-colors.sh
 $HOME/.config/scripts/colors/telegram-colors.sh
 #$HOME/.config/scripts/colors/termusic-colors.sh
